94f49815b26cd792e8954b13f64d4514c60de36b
ivangsp <ojiamboivan@gmail.com>
2019-01-28 17:52:37 +0200

    server working

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const path = require('path');
+var path = require('path');
 const movies = require('./movie.mock-data');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
 
 
 const router = express.Router();
